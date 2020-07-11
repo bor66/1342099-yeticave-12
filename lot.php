0d62eb06f8056327ff45f13d83f77c6271141bfb
Aleksandr <bor.88@mail.ru>
2020-07-11 22:27:18 +0500

    module6-task1

--- lot.php
+++ lot.php
@@ -5,26 +5,32 @@ $user_name = 'Aleksandr';
 $title = '';
 
 $con = mysqli_connect("localhost", "root", "", "yeticave");
+
+if ($con == false) {
+    print("Ошибка подключения: " . mysqli_connect_error());
+}
+
 mysqli_set_charset($con, "utf8");
+
+
 
 $lot_id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT); 
 
-$sql_lot = "SELECT l.id, l.name, l.add_date,  l.description, l.image_link, l.start_price, l.expiration_date, l.step_sum, b.sum_price, c.name category 
-FROM lots l JOIN categories c ON c.id = l.category_id
-LEFT JOIN bet b ON b.lot_id = l.id WHERE l.id = '$lot_id'";
-
-$result_lots = mysqli_query($con, $sql_lots);
+$sql_lot = "SELECT l.id, l.name, l.add_date,  l.description, l.image_link, l.start_price, l.expiration_date, l.step_sum, b.sum_price, c.name category FROM lots l JOIN categories c ON c.id = l.category_id LEFT JOIN bet b ON b.lot_id = l.id WHERE l.id = '$lot_id'";
+$result_lots = mysqli_query($con, $sql_lot);
 $rows_lots = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
 
+
 $sql_categories = "SELECT name, code FROM categories";
-$result_lots = mysqli_query($con, $sql_categories);
+$result_categories = mysqli_query($con, $sql_categories);
 $rows_categories = mysqli_fetch_all($result_lots, MYSQLI_ASSOC);
 
-$sql_bet = "SELECT b.bet_date, b.sum_price, b.user_id, b.lot_id, u.name FROM bet b JOIN users u ON b.id = u.id WHERE WHERE b.lot_id = '$lot_id'"
+$sql_bet = "SELECT b.bet_date, b.sum_price, b.user_id, b.lot_id, u.name FROM bet b JOIN users u ON b.id = u.id WHERE l.id = '$lot_id'";
+
 $result_bet = mysqli_query($con, $sql_bet);
 $rows_bet = mysqli_fetch_all($result_bet, MYSQLI_ASSOC);
 
-if (isset ($_GET['id']))
+if (isset ($_GET['id']));
 
 $lot_page_content = include_template('lot_info.php', ['categories' => $rows_categories, 'items' => $rows_lots, 'rows_bet' => $rows_bet]);
 $layout_content = include_template('layout.php', ['page_content' => $page_content, 'user_name' => $user_name, 'title' => $title, 'categories' => $rows_categories, 'is_auth' => $is_auth]);
--- templates/lot_info.php
+++ templates/lot_info.php
@@ -31,25 +31,26 @@
                 Мин. ставка <span><?=htmlspecialchars($value["step_sum"]);?></span>
               </div>
             </div>
-            <?php endforeach; ?>
             <form class="lot-item__form" action="https://echo.htmlacademy.ru" method="post" autocomplete="off">
               <p class="lot-item__form-item form__item form__item--invalid">
                 <label for="cost">Ваша ставка</label>
-                <input id="cost" type="text" name="cost" placeholder="12 000">
+                <input id="cost" type="text" name="cost" placeholder="<?=htmlspecialchars($value["step_sum"]);?>">
                 <span class="form__error">Введите наименование лота</span>
               </p>
               <button type="submit" class="button">Сделать ставку</button>
             </form>
           </div>
+          <?php endforeach; ?>
           <div class="history">
-            <h3>История ставок (<span>10</span>)</h3>
+            <h3>История ставок (<span><?= count($bet);?></span>)</h3>
             <table class="history__list">
+            <?php foreach ($rows_bet as $key => $value) :?>
               <tr class="history__item">
-                <td class="history__name">Иван</td>
-                <td class="history__price">10 999 р</td>
-                <td class="history__time">5 минут назад</td>
+                <td class="history__name"><?= htmlspecialchars($value["name"]); ?></td>
+                <td class="history__price"><?= get_cost(htmlspecialchars($value["sum_price"])); ?></td>
+                <td class="history__time"><?= htmlspecialchars($value["bet_date"]); ?></td>
               </tr>
-              
+              <?php endforeach; ?>
             </table>
           </div>
         </div>
